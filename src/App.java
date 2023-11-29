46552b8ec8267fd4a7129fc239dab1403e3e8f38
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-11-30 00:25:35 +0530

    created method to insert node

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Binary Search Tree Program.");
+
+        // UC1: creating binary tree and adding nodes
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
+        bst.insert(56);
+        bst.insert(30);
+        bst.insert(70);
+
+        System.out.print("\nInorder Traversal: ");
+        bst.inorder();
     }
 }
---
+++ src/BinarySearchTree.java
@@ -0,0 +1,47 @@
+public class BinarySearchTree<T extends Comparable<T>> {
+    Node<T> root;
+    int size;
+
+    public BinarySearchTree() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    // UC1: method to add node to BST
+    public void insert(T data) {
+        root = this.insertHelper(this.root, data);
+        this.size++;
+    }
+
+    // helper function for insert method
+    private Node<T> insertHelper(Node<T> root, T data) {
+        if (root == null) {
+            root = new Node<>(data);
+            return root;
+        }
+
+        if (data.compareTo(root.data) < 0) {
+            root.left = this.insertHelper(root.left, data);
+        } else {
+            root.right = this.insertHelper(root.right, data);
+        }
+
+        return root;
+    }
+
+    // inorder traversal to print the nodes
+    public void inorder() {
+        this.inorderHelper(this.root);
+    }
+
+    // helper function for inorder traversal
+    private void inorderHelper(Node<T> root) {
+        if (root == null) {
+            return;
+        }
+
+        inorderHelper(root.left);
+        System.out.print(root.data + " ");
+        inorderHelper(root.right);
+    }
+}
